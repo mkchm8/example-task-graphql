01a36de78b886d2e34fedb4083c03b81fd9781a9
mkchm8 <8792739+mkchm8@users.noreply.github.com>
2023-12-28 17:06:21 +0900

    開発用のシーダーを作成 #8

---
+++ database/seeders/DevelopSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Folder;
+use App\Models\Tag;
+use App\Models\Task;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DevelopSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        User::factory()
+            ->has(
+                Folder::factory(3)->has(
+                    Task::factory(3)->has(
+                        Tag::factory(2)
+                    )
+                )
+            )
+            ->create();
+    }
+}
