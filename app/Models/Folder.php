5f2b95bff36a6cf98357a09554d9bcc0bd86bea7
mkchm8 <8792739+mkchm8@users.noreply.github.com>
2023-12-28 17:36:48 +0900

    Folderモデルのリレーション修正 #10

--- app/Models/Folder.php
+++ app/Models/Folder.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Carbon;
 
@@ -17,6 +19,7 @@ use Illuminate\Support\Carbon;
  * @property Carbon|null $deleted_at
  * @property Carbon $created_at
  * @property Carbon $updated_at
+ * @property Collection<Task> $tasks
  */
 class Folder extends Model
 {
@@ -39,5 +42,13 @@ class Folder extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    /**
+     * @return HasMany<Task>
+     */
+    public function tasks(): HasMany
+    {
+        return $this->hasMany(Task::class);
     }
 }
